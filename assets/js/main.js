6d4b0c16702323c7ec14de542e721acc5303fd14
andrejblesic <blesic.andrej@gmail.com>
2021-03-09 11:48:15 +0100

    added carousels

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,6 +24,57 @@ $(document).ready(() => {
 
     console.log($(window).width());
 
+    let portfolioOwl = $('.owl-carousel.portfolio-items-wrapper').owlCarousel({
+        items: 1,
+        loop: true
+    });
+
+    $('.portfolio-next').on('click', function() {
+        portfolioOwl.trigger('next.owl.carousel');
+    });
+
+    $('.portfolio-prev').on('click', function() {
+        portfolioOwl.trigger('prev.owl.carousel');
+    });
+
+    let newsOwl = $('.owl-carousel.news-items-wrapper').owlCarousel({
+        items: 1,
+        loop: true
+    });
+
+    $('.news-next').on('click', function() {
+        newsOwl.trigger('next.owl.carousel');
+    });
+
+    $('.news-prev').on('click', function() {
+        newsOwl.trigger('prev.owl.carousel');
+    });
+
+    let desktopPortfolioOwl = $('.owl-carousel.desktop-portfolio-owl').owlCarousel({
+        items: 3,
+        dotsEach: true,
+        loop: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            767: {
+                items: 2
+            },
+            1024: {
+                items: 3
+            }
+        }
+    })
+    
+    $('.desktop-portfolio-next').on('click', function() {
+        desktopPortfolioOwl.trigger('next.owl.carousel');
+    });
+
+    $('.desktop-portfolio-prev').on('click', function() {
+        desktopPortfolioOwl.trigger('prev.owl.carousel');
+    });
+
     // if ($(window).width() > 767) {
     //     setTimeout(() => {
     //         const scroll = new LocomotiveScroll({
